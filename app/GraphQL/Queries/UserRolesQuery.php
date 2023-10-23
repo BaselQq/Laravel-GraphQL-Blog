831345574b6017f38a7d6983bae126a1e1c3ef8c
baselqq <baselqarb@gmail.com>
2023-10-24 01:11:41 +0200

    - added user roles graphql type

---
+++ app/GraphQL/Queries/UserRolesQuery.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\Models\Role;
+use GraphQL\Type\Definition\Type;
+use GraphQL\Type\Definition\Type as GraphQLType;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Query;
+
+class UserRolesQuery extends Query {
+
+    protected $attributes = [
+        'name' => 'userRoles'
+    ];
+
+    public function type(): GraphQLType {
+        return Type::listOf(GraphQL::type('UserRole'));
+    }
+
+    public function resolve($root, $args) {
+        return Role::all();
+    }
+}
